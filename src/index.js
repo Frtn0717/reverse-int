22981a1684eea920d6ebd35037c775a53c0dadb0
Stanislau <pontty232@gmail.com>
2020-10-19 23:58:04 +0300

    feat: add function reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function reverse (n) {
-  
+
+  if (n > 0) {
+    let strNum = String(n);
+    let result = '';
+
+    for (let i = strNum.length; i >= 0; i-- ) {
+    result = result + strNum.charAt(i);
+    }
+    return result;
+  } else {
+    n = -n;
+    let strNum = String(n);
+    let result = '';
+
+    for (let i = strNum.length; i >= 0; i-- ) {
+        result = result + strNum.charAt(i);
+    }
+    return result;
+  }
 }
